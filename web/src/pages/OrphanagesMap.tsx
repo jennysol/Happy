dd8a7abb12eb1fad2f21ae87b1006706fac6342b
jennysol <jennyssoliver@gmail.com>
2020-10-13 11:14:06 -0300

    Adicionando Routes

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../asserts/img/map-marker.svg';
+
+function OrphanagesMap() {
+  return(
+    <div id="page-map">
+        <aside>
+            <header>
+                <img src={mapMarkerImg} alt="Happy" />
+
+                <h2>Escolha um orfanato no mapa</h2>
+                <p>Muitas crianças estão esperando a sua  visita :) </p>
+            </header>
+
+            <footer>
+                <strong>Brasília</strong>
+                <span>Distrito Federal</span>
+            </footer>
+        </aside>
+
+        <div></div>
+        <Link to="" className="creare-orphanage">
+            <FiPlus size={32} color="#fff" />
+        </Link>
+
+    </div>
+  );
+}
+
+export default OrphanagesMap;
