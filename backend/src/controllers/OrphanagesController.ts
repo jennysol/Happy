d3c4d3e19e61eee786310f4caebd26c912ce9956
jennysol <jennyssoliver@gmail.com>
2020-10-14 20:22:27 -0300

    Finalizando relacionamento de images com o banco

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -23,6 +23,9 @@ export default {
     },
 
     async create(request: Request, response: Response) {
+        console.log(request.files);
+
+
         const {
             name,
             latitude,
@@ -34,6 +37,12 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -43,6 +52,7 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
