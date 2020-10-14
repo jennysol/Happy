36e9962a40396d469d3a8257557cfb631b2c7e92
jennysol <jennyssoliver@gmail.com>
2020-10-14 20:45:50 -0300

    Adicionando Views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,13 +1,16 @@
 import { Request, Response} from 'express';
 import { getRepository } from 'typeorm';
 
+import orphanageView from '../views/orphanages_view';
 import Orphanage from '../models/Orphanage';
 
 export default {
     async index(request: Request, response: Response){
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
 
         return response.json(orphanages);
     },
@@ -17,9 +20,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
 
     async create(request: Request, response: Response) {
---
+++ backend/src/views/orphanages_view.ts
@@ -0,0 +1,17 @@
+import Orphanage from '../models/Orphanage';
+import orphanage from '../models/Orphanage';
+
+export default {
+  render(orphanage: Orphanage) {
+    return {
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,  
+    };
+  }
+};
