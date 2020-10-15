6ae89702cd019129adeca74eea02b74ec7e0bc15
jennysol <jennyssoliver@gmail.com>
2020-10-15 11:07:38 -0300

    Configurando Routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,14 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
-function Routes() {
-    return (
-        <BrowserRouter>
-          <Route path="/" exact component={Landing} />
-          <Route path="/app" component={OrphanagesMap} />
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
-        </BrowserRouter>
-    );
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+
+        <Route path="/orphanage/create" exact component={CreateOrphanage} />
+        <Route path="/orphanage/:id" component={Orphanage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
